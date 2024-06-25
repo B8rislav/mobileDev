0f296fd84539703cca10555c805182108259503c
B8rislav <borislav.bondarev@gmail.com>
2024-06-26 01:18:27 +0300

    feat: image implemented, sizes shortened

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,5 +1,31 @@
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function TabTwoScreen() {
-  return <Text>Under development!</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Explore!</Text>
+      <Image
+        style={styles.image}
+        source={require('./static/Blog-Edits.jpg')}
+        alt="random image"
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 40,
+    backgroundColor: '#e6e6e6',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  image: {
+    width: 320,
+  },
+});
